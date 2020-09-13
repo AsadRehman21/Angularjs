dc4bd001eb499ec45ad8b7e370c6d3a10d063273
AsadRehman21 <57071642+AsadRehman21@users.noreply.github.com>
2020-09-13 15:33:59 +0500

    First Assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular.module('DIApp', [])
+
+    .controller('DIController', DIController);
+
+    function DIController($scope, $filter) {
+        $scope.name = "Asad";
+        $scope.upper = function() {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
+
+
+
+    }
+
+
+})();
