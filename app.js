0743b202574f90976e237ce933fb5f5a810dccf3
AsadRehman21 <57071642+AsadRehman21@users.noreply.github.com>
2020-09-16 13:17:35 +0500

    Complete Assignmnet 1

--- app.js
+++ app.js
@@ -6,10 +6,20 @@
     .controller('DIController', DIController);
 
     function DIController($scope, $filter) {
-        $scope.name = "Asad";
-        $scope.upper = function() {
-            var upCase = $filter('uppercase');
-            $scope.name = upCase($scope.name);
+        $scope.name = "";
+        $scope.message = "";
+        $scope.onClickMe = function() {
+            var nameArr = $scope.name.split(',').join('');
+
+            if (nameArr.length == 0) {
+                $scope.message = "Please enter data first";
+            } else if (nameArr.length < 4 && nameArr.length != 0) {
+                $scope.message = "Enjoy";
+
+            } else if (nameArr.length > 3) {
+                $scope.message = "Too Much";
+            }
+
         };
 
 
