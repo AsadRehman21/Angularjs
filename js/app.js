1f70ed0fb5a047dcdfa2236a92fe5d215c3e6a08
AsadRehman21 <57071642+AsadRehman21@users.noreply.github.com>
2020-09-17 12:10:05 +0500

    Complete Assignmnet 1

--- js/app.js
+++ js/app.js
@@ -6,17 +6,19 @@
     .controller('DIController', DIController);
 
     function DIController($scope, $filter) {
-        $scope.name = "";
+        $scope.name = '';
         $scope.message = "";
         $scope.onClickMe = function() {
-            var nameArr = $scope.name.split(',').join('');
+            var nameArr = $scope.name.split(',');
+            if ($scope.name == '') {
 
-            if (nameArr.length == 0) {
                 $scope.message = "Please enter data first";
-            } else if (nameArr.length < 4 && nameArr.length != 0) {
+
+            } else if (nameArr.length <= 3 && nameArr.length >= 1) {
                 $scope.message = "Enjoy";
 
             } else if (nameArr.length > 3) {
+
                 $scope.message = "Too Much";
             }
 
